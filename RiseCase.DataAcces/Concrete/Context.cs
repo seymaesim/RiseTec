06887a157127aa958206c946d8250aee85f7febf
şeyma esim <esim794@gmail.com>
2023-01-15 22:15:37 +0300

    Project Npgsq connection

--- RiseCase.DataAcces/Concrete/Context.cs
+++ RiseCase.DataAcces/Concrete/Context.cs
@@ -22,7 +22,7 @@ namespace RiseCase.DataAcces.Concrete
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=localhost; Database=dbRiseTec; Port=5432; User Id=postgres; Password=sa");
+            optionsBuilder.UseNpgsql("Host=localhost; Database=dbRiseTec; Port=5432; User Id=postgres; Password=*");
         }
 
         public DbSet<User> Users { get; set; } //Kişilerin bilgileri
